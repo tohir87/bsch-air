662453118c7b4cede86c703188b6097e952ca07c
Tohir <otcleantech@gmail.com>
2019-04-25 09:47:27 +0000

    First push

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/catkin_ws/src
+# Build directory: /home/user/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(project_localization)
+subdirs(project_mapping)
+subdirs(project_planning)
